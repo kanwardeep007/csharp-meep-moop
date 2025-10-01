9f129da43fdde881cf53322db819dbe4b1aa9d7e
Kanwardeep <kanwardeep007@gmail.com>
2025-10-01 19:57:53 +0100

    update

--- src/meepMoop/Pet.cs
+++ src/meepMoop/Pet.cs
@@ -95,7 +95,7 @@ namespace meepMoop
     {
         public SDKConfig SDKConfiguration { get; private set; }
         private const string _language = "csharp";
-        private const string _sdkVersion = "0.0.3";
+        private const string _sdkVersion = "0.0.4";
         private const string _sdkGenVersion = "2.716.4";
         private const string _openapiDocVersion = "1.0.0";
 
